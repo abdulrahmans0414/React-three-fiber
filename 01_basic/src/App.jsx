80f1b345435e15df99d4e496932bf49ba3bc7b63
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-26 18:09:32 +0530

    initial commit setup 01_basic with react three fiber

---
+++ 01_basic/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <p>This is a simple React application using React Three Fiber by Abdul Rahman.</p>
+
+    </div>
+  )
+};
+
+export default App;
