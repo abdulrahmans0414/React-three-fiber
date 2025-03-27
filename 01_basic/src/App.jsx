4a5796e63bbf5bae6f10fa14b9d9b92c6ff7749a
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-27 18:10:53 +0530

    implement useFrame animation for Cube (rotation + sine wave motion)

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -1,11 +1,26 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css'
 
-import React from "react";
+import React, { useRef } from "react";
 
 const Cube = ({ position, size, color }) => {
+
+  const ref = useRef()
+  useFrame((state, delta) => {
+    // console.log(delta);
+    // console.log(state);
+
+    // rotating the cube
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 0.5; // Rotate Y axis at half speed
+
+    // moving the cube
+    // console.log(state.clock.elapsedTime);
+    ref.current.position.x = Math.sin(state.clock.elapsedTime) * 2
+  })
+
   return (
-    <mesh position={position}>
+    <mesh position={position} ref={ref}>
       <boxGeometry args={size} />
       <meshStandardMaterial color={color} />
     </mesh>
@@ -22,12 +37,14 @@ const App = () => {
       {/* This light cant used cast shadows as it does not have a direction */}
       <ambientLight intensity={0.5} />
 
-      <group position={[0, -1, 0]}>
+      {/* <group position={[0, -1, 0]}>
         <Cube position={[1, 0, 0]} size={[1, 1, 1]} color={"hotpink"} />
         <Cube position={[-1, 0, 0]} size={[1, 1, 1]} color={"green"} />
         <Cube position={[1, 2, 0]} size={[1, 1, 1]} color={"skyblue"} />
         <Cube position={[-1, 2, 0]} size={[1, 1, 1]} color={"yellow"} />
-      </group>
+      </group> */}
+
+      <Cube position={[1, 0, 0]} size={[1, 1, 1]} color={"hotpink"} />
 
     </Canvas>
   )
