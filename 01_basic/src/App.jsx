ee4258fba69f0c0710b10a05a4565000fcd4ba5c
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-27 17:48:44 +0530

    add enhance scene with ambient lighting and colored cubes

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -16,12 +16,16 @@ const App = () => {
   return (
     <Canvas>
       {/* add lighting for meshStandardMaterial */}
-      <directionalLight position={[0, 0, 2]} />
+      <directionalLight position={[0, 0, 2]} intensity={0.5} />
+
+      {/* ambientLight -> is a global light*/}
+      {/* This light cant used cast shadows as it does not have a direction */}
+      <ambientLight intensity={0.5} />
 
       <Cube position={[1, 0, 0]} size={[1, 1, 1]} color={"hotpink"} />
-      <Cube position={[-1, 0, 0]} size={[1, 1, 1]} color={"hotpink"} />
-      <Cube position={[1, 2, 0]} size={[1, 1, 1]} color={"hotpink"} />
-      <Cube position={[-1, 2, 0]} size={[1, 1, 1]} color={"hotpink"} />
+      <Cube position={[-1, 0, 0]} size={[1, 1, 1]} color={"green"} />
+      <Cube position={[1, 2, 0]} size={[1, 1, 1]} color={"skyblue"} />
+      <Cube position={[-1, 2, 0]} size={[1, 1, 1]} color={"yellow"} />
 
     </Canvas>
   )
