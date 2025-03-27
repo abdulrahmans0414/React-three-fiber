a34ea4c5df9f1dbbbbd4462dcec5bc5f619425c3
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-27 17:24:10 +0530

    add directional light for meshStandardMaterial

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -7,11 +7,17 @@ import React from "react";
 const App = () => {
   return (
     <Canvas>
+      {/* add lighting for meshStandardMaterial */}
+      <directionalLight position={[1, 1, 1]} />
+
+
       <mesh>
         <boxGeometry args={[2, 2, 4]} />   {/* args = [x, y, z] for size*/}
-        <meshBasicMaterial />
-        {/* <meshStandardMaterial /> */}
+        {/* <meshBasicMaterial /> */}
+        <meshStandardMaterial color={"hotpink"} />
       </mesh>
+
+
 
     </Canvas>
   )
