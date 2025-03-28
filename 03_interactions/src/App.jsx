b7b15386b4ef5746b0d79c7d3e4eed67f0bc1c14
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-28 06:33:27 +0530

    add interactive sphere with hover and click effects

--- 03_interactions/src/App.jsx
+++ 03_interactions/src/App.jsx
@@ -1,11 +1,43 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css'
+import { useRef, useState } from 'react';
 
+const Sphare = ({ position, size, color }) => {
+  const ref = useRef()
+
+  // Hovered effect on sphere
+  const [isHovered, setIsHover] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
+
+  useFrame((state, delta) => {
+    const speed = isHovered ? 1 : 0.2
+    // rotating the cube
+    ref.current.rotation.y += delta * speed;
+  })
+
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerOver={(e) => (e.stopPropagation(), setIsHover(true))}
+      onPointerOut={() => setIsHover(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}
+
+    >
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={isHovered ? "orange" : color} wireframe />
+    </mesh>
+  )
+}
 function App() {
 
   return (
     <Canvas>
+      <directionalLight position={[0, 0, 2]} intensity={0.5} />
+      <ambientLight intensity={0.5} />
 
+      <Sphare position={[0, 0, 0]} size={[1, 32, 32]} color={"lightblue"} />
     </Canvas>
   )
 }
